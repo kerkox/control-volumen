aa384b975fe6a8e7675765326451707ceaf5956b
Paul Cortes <paulkerg@gmail.com>
2020-12-19 15:00:05 -0500

    agregado todo

---
+++ main.js
@@ -0,0 +1,53 @@
+const { exec } = require('child_process');
+const qs = require('qs')
+const fastify = require('fastify')({
+  logger: true,
+  querystringParser: str => qs.parse(str)
+});
+
+fastify.get('/', function (request, reply) {
+  reply.send({ hello: 'world' })
+})
+
+const opts = {
+  schema: {
+    querystring: {
+      value: { type: 'integer' }
+    },
+    response: {
+      200: {
+        type: 'object',
+        properties: {
+          ok: { type: 'boolean' }
+        }
+      }
+    }
+  },
+}
+
+fastify.get('/volumen/:value', opts,
+  (req, reply) => {
+    console.log('req.params',req.params)
+    let value = req.params.value;
+    console.log(`value of volumen= ${value}`);
+    const command = `libs\\nircmd.exe changesysvolume ${value}`;
+    console.log(`command: ${command}`)
+    exec(command, (err, stdout, sterr) => {
+      if (err) {
+        console.error(err);
+        reply.send({ ok: false })
+        return false;
+      }
+      console.log(stdout)
+      reply.send({ ok: true })
+    })
+  })
+
+
+fastify.listen(3000,'0.0.0.0', function (err, address) {
+  if (err) {
+    fastify.log.error(err);
+    process.exit(1);
+  }
+  fastify.log.info(`server listening on ${address}`)
+})
